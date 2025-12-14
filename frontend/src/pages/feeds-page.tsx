dcaca2a6e64cdc16163170741fc4b24c5928c9b5
Veeti K <veeti@veetik.com>
2025-12-14 15:55:29 +0200

    feeds list empty state

--- frontend/src/pages/feeds-page.tsx
+++ frontend/src/pages/feeds-page.tsx
@@ -70,44 +70,48 @@ function FeedsListSkeleton() {
 
 function FeedsList(props: { feeds?: Array<Feed> }) {
 	return (
-		<ul class="flex flex-col gap-1">
-			<For each={props.feeds}>
-				{(feed) => (
-					<li class="focus:bg-gray-a2 hover:bg-gray-a2 group/feed relative -mx-4 flex flex-col gap-2 p-4">
-						<a
-							href={`/feeds/${feed.id}`}
-							class="focus absolute top-0 left-0 h-full w-full"
-						></a>
-						<div class="flex items-center gap-3">
-							<img
-								class="inline-flex size-6"
-								src={API_BASE_URL + `/v1/feeds/${feed.id}/icon`}
-							/>
+		<>
+			{!props.feeds?.length ? (
+				<p class="bg-gray-a2/60 p-4">No feeds yet</p>
+			) : (
+				<ul class="flex flex-col gap-1">
+					{props.feeds?.map((feed) => (
+						<li class="focus:bg-gray-a2 hover:bg-gray-a2 group/feed relative -mx-4 flex flex-col gap-2 p-4">
+							<a
+								href={`/feeds/${feed.id}`}
+								class="focus absolute top-0 left-0 h-full w-full"
+							></a>
+							<div class="flex items-center gap-3">
+								<img
+									class="inline-flex size-6"
+									src={API_BASE_URL + `/v1/feeds/${feed.id}/icon`}
+								/>
 
-							<div class="flex items-center gap-2 font-medium">
-								<span class="font-cool inline text-[1.3rem] group-hover/feed:underline group-has-[a[id=site]:hover]/feed:no-underline">
-									{feed.title}
-								</span>
-								<a
-									id="site"
-									href={feed.site_url}
-									class="group/link text-gray-11 relative z-10 -m-4 p-4 text-xs outline-none"
-								>
-									<span class="in-focus:outline-gray-a10 underline group-hover/link:text-white in-focus:outline-2 in-focus:outline-offset-2 in-focus:outline-none in-focus:outline-solid">
-										{feed.site_url
-											.replace(/^https?:\/\//, "")
-											.replace(/\/$/, "")}
+								<div class="flex items-center gap-2 font-medium">
+									<span class="font-cool inline text-[1.3rem] group-hover/feed:underline group-has-[a[id=site]:hover]/feed:no-underline">
+										{feed.title}
 									</span>
-								</a>
+									<a
+										id="site"
+										href={feed.site_url}
+										class="group/link text-gray-11 relative z-10 -m-4 p-4 text-xs outline-none"
+									>
+										<span class="in-focus:outline-gray-a10 underline group-hover/link:text-white in-focus:outline-2 in-focus:outline-offset-2 in-focus:outline-none in-focus:outline-solid">
+											{feed.site_url
+												.replace(/^https?:\/\//, "")
+												.replace(/\/$/, "")}
+										</span>
+									</a>
+								</div>
 							</div>
-						</div>
 
-						<p class="text-gray-11 text-sm">
-							{feed.entry_count} entries ({feed.unread_entry_count} unread)
-						</p>
-					</li>
-				)}
-			</For>
-		</ul>
+							<p class="text-gray-11 text-sm">
+								{feed.entry_count} entries ({feed.unread_entry_count} unread)
+							</p>
+						</li>
+					))}
+				</ul>
+			)}
+		</>
 	);
 }
